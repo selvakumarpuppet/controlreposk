817b10220e9cf0c1c6ec6bdbb5676d675c8c5750
kumar <kumar@puppet.com>
2025-04-03 10:38:54 +0800

    updated the plan with notify statement

---
+++ internal/bar_tasks_plans/plans/noshow.pp
@@ -0,0 +1,13 @@
+plan bar_tasks_plans::noshow (
+  TargetSpec $targets
+) {
+
+  run_task('bar_tasks_plans::helloworld', $targets)
+
+  $api_key = lookup('bar_tasks_plans::api_key')
+  #notify {"Running with api_key ${api_key} ID defined":}
+
+  #$result  = run_task('mkcustomfact', $targets)
+  $result  = run_task('bar_tasks_plans', $targets, 'api_key' => $api_key)
+  return $result
+}
